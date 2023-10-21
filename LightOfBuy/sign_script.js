b0b9ea60eb5329304e8a910ca810394c06b822bc
Fares-Anwer <143360028+Fares-Anwer@users.noreply.github.com>
2023-10-21 02:46:21 +0000

    some change

---
+++ LightOfBuy/sign_script.js
@@ -0,0 +1,13 @@
+let userInput = document.querySelector("[name='username']");
+let passInput = document.querySelector("[name='password']");
+let link = document.getElementsByClassName("alink");
+
+document.forms[0].onsubmit = function (e) {
+
+};
+
+
+document.links[0].onclick = function (event) {
+    console.log(event);
+    event.pre;
+};
